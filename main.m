31f57da61993f7d67c6c5249637e4974aac26217
stanwong <stanwong178@gmail.com>
2026-01-25 23:49:04 -0800

    30 dB diff

--- main.m
+++ main.m
@@ -3,124 +3,122 @@ clear;
 clc;
 
 c = 299792458; 
-lambda_center = 1550e-9;      % Reference Center 
-lambda_sig    = 1550.6e-9;      % Incoherent Source Center
-width_sig_Hz  = 10e6;         % 10 MHz FWHM
+lambda_center = 1550e-9;      
 f_center      = c/lambda_center;
-f_sig_abs     = c/lambda_sig;
-f_sig_rel     = f_sig_abs - f_center; % Relative freq
 
+f_sig_rel = 20.37e9 ;
+f_sig = f_center + f_sig_rel;
+width_sig_Hz  = 10e6;         % 10 MHz FWHM
 % --- Comb Parameters ---
-frep_1 = 27.2e9;
-frep_2 = 27.201e9;
-d_frep     = frep_2 - frep_1; % 1 MHz difference
-n_lines    = 21;                  % Total lines
-n_indices  = -10:10;              % Indices centered at 0
+frep_1 = 10.2e9;
+frep_2 = 10.3e9;
+d_frep     = frep_2 - frep_1;   % 100 MHz difference
+N_combline    = 11;             % Number of comb lines
+n_indices  = -5:5;              % Indices centered at 0
 
 % --- sampling batch setup ---
 % to prevent aliasing, 1 / dt > 2 f_max = 2 ( 0.5 frep) 
 % NT * Nt *dt = 1 / d_frep = 1ms
-dt = 4e-12;
-Fs = 1 / dt;
-Nt = 1000; % number of time step in a batch
-NT = 250; % number of time batch 
+
+Fs = 120e9;
+dt = 1 / Fs;
+Nt = 512; % number of time step in a batch
+NT = 200; % number of time batch 
 T_batch = Nt *dt; % sampling time for a batch
-d_fRF = 1 / T_batch;
-T = NT * Nt *dt; % total sampling time
+d_fRF = 1 / T_batch; %spectral resolution
+T = NT * T_batch;
 t = 0:dt:T-dt;
 
-%% Generate Combs
-
+% --- Create Combs ---
 comb1 = zeros(size(t));
 comb2 = zeros(size(t));
-
 for n = n_indices
     fn1 = n * frep_1;
     fn2 = n * frep_2;
     comb1 = comb1 + exp(1i * 2*pi * fn1 * t);
     comb2 = comb2 + exp(1i * 2*pi * fn2 * t);
 end
+% Normalize
+E_c1 = comb1 / max(abs(comb1));
+E_c2 = comb2 / max(abs(comb2));
 
-comb1 = comb1 / max(abs(comb1));
-comb2 = comb2 / max(abs(comb2));
-
-%% Generate Incoherent Source
+% --- Create Incoherent Signal ---
 noise = (randn(size(t)) + 1i*randn(size(t)));
-signal = noise .* exp(1i * 2*pi * f_sig_rel * t);
-envelope = signal .* exp(-1i * 2*pi * f_sig_rel * t);
-fc_lpf = width_sig_Hz; 
-[b, a] = butter(1, fc_lpf/(Fs/2));
+envelope = noise; 
+[b, a] = butter(1, width_sig_Hz/(Fs/2));
 envelope_filtered = filter(b, a, envelope);
 signal = envelope_filtered .* exp(1i * 2*pi * f_sig_rel * t);
-signal = 0.5 * signal / mean(abs(signal)); % scaling
+E_s = 2 * signal / mean(abs(signal)); 
 
-%% Interference
-
-E1 = comb1 + signal;
-E2 = comb2 + signal;
-plot_beatnotes(comb1,comb2, signal,dt);
-
-I1 = abs(E1).^2;
-I2 = abs(E2).^2;
-
-% Remove DC (High-pass filter or subtract mean) to keep only beats
-I1 = I1 - mean(I1);
-I2 = I2 - mean(I2);
-
-%% create frequency x time spectrograms, each time step is a sampling batch
-I1_mat = reshape(I1, Nt, NT);
-I2_mat = reshape(I2, Nt, NT);
-
-% FFT each time batch to get spectrograms
-F1 = fftshift(fft(I1_mat, [], 1),1);
-F2 = fftshift(fft(I2_mat, [], 1),1);
+S1 = conj(E_c1) .* E_s;
+S2 = conj(E_c2) .* E_s;
 
 
-%% Ptychoscopy inversion
-% Define RF Frequency Axis
+% plot_complex(S1,Fs);
+% plot_beatnotes(S1,S2,dt);
+%%
+S1 = reshape(S1, Nt, NT);
+S2 = reshape(S2, Nt, NT);
+
 f_RF = (-Nt/2 : Nt/2 - 1) * d_fRF;
-batch_index = 1 : NT;
+omega_k = 2 * pi * f_RF;
 
+ti = (0:Nt-1).' * dt;
+F1 = (1/Nt) * (exp(-1i * omega_k.' * ti.') * S1);
+F2 = (1/Nt) * (exp(-1i * omega_k.' * ti.') * S2);
+%%
+max_freq_span = (max(n_indices) - min(n_indices)) * frep_1 + 2*Fs; % buffer
+total_bins    = ceil(max_freq_span / d_fRF);
+N_bins_linespan = ceil(frep_1 / d_fRF);
 
-% Optical Axis: Wide enough to see multiple ghosts (-200 to +100 GHz)
-f_opt_axis = linspace(-200e9, 200e9, 100000);
-spec_recon = zeros(size(f_opt_axis));
+% Initialize global spectrum
+spec_recon = zeros(total_bins, 1);
+center_idx = floor(total_bins / 2);
+global_freq_axis = ((1:total_bins).' - center_idx) * d_fRF + f_center;
 
 
 for n = n_indices
-    Cn = zeros(size(F1(:,1)));
-    delta_n = n * d_frep;
-    shift_bins = round(delta_n / d_fRF); 
+    Cn = zeros(Nt, 1);
 
-    for j = batch_index
+    delta_n = n * d_frep;
+    shift_bins = round(delta_n / d_fRF);
+
+    for j = 1:NT
         F1_n = F1(:,j);
         F2_n = circshift(F2(:,j), shift_bins, 1);
         Tj = (j-1) * T_batch;
-        phasor = exp(1i * 2*pi * delta_n * Tj);
+        phasor = exp(1i * 2 * pi * delta_n * Tj);
         Cn  = Cn + F2_n .* conj(F1_n) .* phasor;
+        
     end
     Cn = Cn / NT;
-    
-    f_comb_line = n * frep_1;
-    f_current_grid = f_comb_line + f_RF;
-    
+
    
-    spec_recon = spec_recon + interp1(f_current_grid, abs(Cn), f_opt_axis, 'linear', 0);
+    freq_offset = n * frep_1;
+    bin_offset_global = round(freq_offset / d_fRF);
+    target_center_idx = center_idx + bin_offset_global;
+
+    % Recalculate global indices 
+    global_start_idx = target_center_idx - floor(N_bins_linespan/2);
+    global_end_idx   = global_start_idx + N_bins_linespan - 1;
+    
+    % Recalculate local Cn indices (Centered window)
+    Cn_start_idx = floor(Nt/2) - floor(N_bins_linespan/2) + 1;
+    Cn_end_idx   = Cn_start_idx + N_bins_linespan - 1;
+    
+    if global_start_idx > 0 && global_end_idx <= total_bins
+        spec_recon(global_start_idx:global_end_idx) = spec_recon(global_start_idx:global_end_idx) + Cn(Cn_start_idx: Cn_end_idx);
+    else
+        warning('Comb line n=%d is outside the spec_recon array bounds.', n);
+    end
 end
 
-%% Plotting
-close all;
-figure('Color', 'w', 'Position', [100 100 1000 400]);
+figure;
+plot(global_freq_axis / 1e12, 20*log10(abs(spec_recon)), "LineWidth",1.5);
+title('Stitched Dual-Comb Spectrum');
+xlabel('Optical Frequency (THz)');
+ylabel('Signal Power (dB)')
 
-% Plot Reconstruction
-plot(f_opt_axis/1e9, spec_recon, 'k', 'LineWidth', 1.5); hold on;
-
-% Plot Ground Truth (The Signal)
-xline(f_sig_rel/1e9, '-r', 'True Signal', 'LineWidth', 2);
-
-title('Reconstruction result');
-xlabel('Optical Frequency (GHz)');
-ylabel('Accumulated Magnitude');
-xlim([-130, 130]); 
-set(gca, "Fontsize",18);
-grid on;
+xline(f_center / 1e12,'--b', "Center freq");
+xline(f_sig / 1e12,'--r', "Signal freq");
+set(gca,"Fontsize",18)
--- plot_beatnotes.m
+++ plot_beatnotes.m
@@ -1,19 +1,10 @@
-function analyze_beat_notes(comb1, comb2, signal,dt)
+function analyze_beat_notes(S1, S2, dt)
 
     %% 1. Simulate Photodetection (Intensity)
     % The detector measures Power ~ |E|^2
-    I1 = abs(comb1+signal).^2;
-    I11 = abs(comb1).^2;
-    I2 = abs(comb2+signal).^2;
-    I22 = abs(comb2).^2;
+    I1 = abs(S1).^2;
+    I2 = abs(S2).^2;
 
-    % Remove DC component (Mean) to visualize AC beat notes clearly
-    I1 = I1 - mean(I1);
-    I2 = I2 - mean(I2);
-    I11 = I11 - mean(I11);
-    I22 = I22 - mean(I22);
-
-    %% 2. Compute FFT
     L = length(I1);
     Fs = 1/dt;
     
@@ -21,21 +12,19 @@ function analyze_beat_notes(comb1, comb2, signal,dt)
     frequencies = (-L/2 : L/2 - 1) * (Fs/L);
     
     % FFT (Normalized)
-    spec_I1 = abs(fftshift(fft(I1)) - fftshift(fft(I11))) / L;
-    spec_I2 = abs(fftshift(fft(I2))- fftshift(fft(I22))) / L;
+    spec_I1 = abs(fftshift(fft(I1))) / L;
+    spec_I2 = abs(fftshift(fft(I2))) / L;
 
     %% 3. Plotting
     figure('Name', 'Dual-Comb Intermediate Spectrum Analysis', 'Color', 'w', 'Position', [100, 100, 1000, 800]);
 
-    plot(frequencies/1e9, spec_I1, 'b', 'LineWidth', 1.2); 
+    plot(frequencies/1e9, 20*log10(spec_I1), 'b', 'LineWidth', 1.2); 
     hold on;
-    plot(frequencies/1e9, spec_I2, 'r', 'LineWidth', 1.2);
+    plot(frequencies/1e9, 20*log10(spec_I2), 'r', 'LineWidth', 1.2);
     % visual formatting
     set(gca,"FontSize",18);
-    title('E1');
     xlabel('Frequency (GHz)');
     ylabel('Magnitude (a.u.)');
     grid on;
-    xlim([-12 12])
     
 end
