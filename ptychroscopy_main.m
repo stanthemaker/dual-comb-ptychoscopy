e3951fb58e10136194ff957c399a5c31e0888db9
stanwong <stanwong178@gmail.com>
2026-03-11 14:55:53 -0700

    incoherent source work

--- ptychroscopy_main.m
+++ ptychroscopy_main.m
@@ -6,8 +6,6 @@ c = 299792458;
 lambda_center = 1550e-9;      
 f_center      = c/lambda_center;
 
-f_sig_rel = 30e9 ;
-f_sig = f_center + f_sig_rel;
 
 % --- Comb Parameters ---
 frep_1 = 27.2e9;
@@ -21,17 +19,18 @@ delta_n_min = abs(d_frep) * min(n_indices) + comb2_shift;
 
 Fs = 200e9;
 dt = 1 / Fs;
-T_batch = 4e-9; 
+T_batch = 2e-9; 
 Nt = round(T_batch / dt);
 d_fRF = 1 / T_batch;
 
 T_ifgm = 1 / abs(d_frep);
-k = 20;                         % 100 full period
+k = 50;                         % 100 full period
 T_total = k * T_ifgm;
 df = 1/ T_total;
 NT = round(T_total / T_batch);
 
 t = 0:dt:T_total-dt;
+N_total = length(t);
 
 % --- Create Combs ---
 E_c1 = zeros(size(t));
@@ -49,21 +48,29 @@ E_c2 = E_c2 / max(abs(E_c2));
 % --- Create coherent single-wavelength signal ---
 % E_s = exp(1i * 2*pi * f_sig_rel * t);
 % E_s = E_s / max(abs(E_s)); 
-
-rect_width = 0.8e9;
-f_vec = (f_sig_rel : df : f_sig_rel+rect_width).';
-spectral_phases = exp(1i * 2 * pi * rand(length(f_vec), 1)); 
-E_s = sum(exp(1i * 2 * pi * f_vec * t) .* spectral_phases, 1);
-E_s = E_s / max(abs(E_s));
+% rect_width = 2e9;
+% N_total = length(t); 
+% df_exact = Fs / N_total;
+% idx_start = round(f_sig_rel / df_exact) + 1;
+% idx_end   = round((f_sig_rel + rect_width) / df_exact) + 1;
+% 
+% S_f = zeros(1, N_total);
+% num_bins = idx_end - idx_start + 1;
+% S_f(idx_start:idx_end) = exp(1i * 2 * pi * rand(1, num_bins));
+% E_s = ifft(S_f);
+% E_s = E_s / max(abs(E_s));
 
 % --- Create Incoherent Signal ---
-% width_sig_Hz  = 10e6;           % 10 MHz FWHM
-% noise = (randn(size(t)) + 1i*randn(size(t)));
-% envelope = noise; 
-% [b, a] = butter(1, width_sig_Hz/(Fs/2));
-% envelope_filtered = filter(b, a, envelope);
-% signal = envelope_filtered .* exp(1i * 2*pi * f_sig_rel * t);
-
+f_s = 28e9;
+f_s_global = f_center + f_s;
+BW = 3e9;
+f_axis = (-N_total/2 : N_total/2-1)' * (Fs/N_total);
+filter_gauss = exp(-((f_axis - f_s).^2) / (2 * (BW/2.355)^2)); 
+noise_raw = (randn(N_total, 1) + 1i*randn(N_total, 1));
+Es_fft = fftshift(fft(noise_raw)) .* filter_gauss;
+E_s = ifft(ifftshift(Es_fft));
+E_s = E_s / max(abs(E_s));
+E_s = E_s(:).';
 
 plot_setup(E_c1, E_c2, E_s,Fs);
 %%
@@ -71,112 +78,82 @@ plot_setup(E_c1, E_c2, E_s,Fs);
 I1 = abs(E_s + E_c1).^2 - abs(E_c1).^2 - abs(E_s).^2;
 I2 = abs(E_s + E_c2).^2 - abs(E_c2).^2 - abs(E_s).^2;
 
-cutoff_freq = frep_1 /2;
+cutoff_freq = 0.6 * frep_1; % need to adjust 0.6 depending on n_max ∆frep
 I1 = lowpass(I1, cutoff_freq, Fs);
 I2 = lowpass(I2, cutoff_freq, Fs);
 
 I1 = reshape(I1, Nt, NT);
 I2 = reshape(I2, Nt, NT);
 
-% F1 = fftshift(fft(I1,[],1),1);
-% F2 = fftshift(fft(I2,[],1),1);
-
-%% paper implementation ( need to know the complex field
-% S1 = conj(E_c1) .* E_s + E_c1 .* conj(E_s);
-% S2 = conj(E_c2) .* E_s + E_c2 .* conj(E_s);
-% 
-% f_cutoff = frep_1 / 2; 
-% [b_lp, a_lp] = butter(6, f_cutoff / (Fs/2));
-% S1 = filtfilt(b_lp, a_lp, S1);
-% S2 = filtfilt(b_lp, a_lp, S2);
-% 
-% plot_complex(S1,Fs);
-
-% 
-% S1 = reshape(S1, Nt, NT);
-% S2 = reshape(S2, Nt, NT);
-% 
-% f_RF = (-Nt/2: Nt/2 - 1) * d_fRF;
-% omega_k = 2 * pi * f_RF;
-% 
-% ti = (0:Nt-1).' * dt;
-% F1 = (1/Nt) * (exp(-1i * omega_k.' * ti.') * S1);
-% F2 = (1/Nt) * (exp(-1i * omega_k.' * ti.') * S2);
-
-%%
-max_freq_span = (max(n_indices) - min(n_indices)) * frep_1 + 2*Fs; % buffer
-total_bins    = ceil(max_freq_span / d_fRF);
-N_bins_linespan = ceil(frep_1 / d_fRF);
-
-% Initialize global spectrum
-spec_recon = zeros(total_bins, 1);
-center_idx = floor(total_bins / 2);
-global_freq_axis = ((1:total_bins).' - center_idx) * d_fRF + f_center;
-
+%% getting expected Cn
+% 1. Define resolution based on the FFT padding FIRST
 N_pad = 2^nextpow2(abs(1/d_frep/dt));
 df_pad = (Fs/N_pad);
 f_batch = (-N_pad/2 : N_pad/2-1) * df_pad;
-phasor_sum = 0;
-for n = 1 
-    Cn = zeros(N_pad, 1);
 
+% 2. Initialize global spectrum using the df_pad resolution
+max_n = max(abs(n_indices));
+max_freq_span = 2 * (max_n + 1) * frep_1; 
+total_bins    = ceil(max_freq_span / df_pad);
+
+spec_recon = zeros(total_bins, 1);
+center_idx = floor(total_bins / 2) + 1; % Global baseband DC
+global_freq_axis = ((1:total_bins).' - center_idx) * df_pad + f_center;
+
+% Bins needed for exactly +/- 0.5 * frep_1
+span_bins = round(0.5 * frep_1 / df_pad); 
+Cn_center = N_pad/2 + 1; % 0 Hz bin in the padded baseband FFT
+
+% Calculate local Cn bounds once (Centered around DC)
+Cn_start_idx = Cn_center - span_bins;
+Cn_end_idx   = Cn_center + span_bins - 1;
+
+for n = n_indices
+    Cn = zeros(N_pad, 1);
+    
     % delta_n is defined as f1 - f2
     delta_n = (n * d_frep) - comb2_shift;
     shift_bins = round(delta_n / df_pad);
-
+    
     for j = 1:NT
-        F1 = fftshift(fft(I1(:,j),N_pad ));
+        F1 = fftshift(fft(I1(:,j), N_pad));
         F2 = fftshift(fft(I2(:,j), N_pad));
-        F2_shift = circshift(F2,-shift_bins, 1);
-
+        F2_shift = circshift(F2, -shift_bins, 1);
+        
         Tj = (j-1) * T_batch;
         phasor = exp(-1i * 2 * pi * delta_n * Tj);
-
         Cn  = Cn + F2_shift .* conj(F1) * phasor;
-       
-        % if rem(j, 100) == 0
-        %    figure
-        %    plot(f_batch/1e9, abs(F1),LineWidth=1.5); hold on;
-        %    plot(f_batch/1e9 , abs(F2),"r--",LineWidth=1.5);
-        %    plot(f_batch/1e9 , abs(F2_shift),"g",LineWidth=1.5);
-        %    legend("F1_n","F2_n","F2_n shifted")
-        %    xlim([-5 5])
-        %    pause;
-        %    close all;
-        % end
-
-    end
+        
+    end    
     Cn = Cn / NT;
     % figure
     % plot(f_batch/1e9 , Cn);
     % pause;
     % close all;
 
-    freq_offset = n * frep_1;
-    bin_offset_global = round(freq_offset / d_fRF);
+    freq_offset_opt = n * frep_1;
+    bin_offset_global = round(freq_offset_opt / df_pad);
     target_center_idx = center_idx + bin_offset_global;
-
-    % Recalculate global indices 
-    global_start_idx = target_center_idx - floor(N_bins_linespan/2);
-    global_end_idx   = global_start_idx + N_bins_linespan - 1;
-
-    % Recalculate local Cn indices (Centered window)
-    Cn_start_idx = floor(Nt/2) - floor(N_bins_linespan/2) + 1;
-    Cn_end_idx   = Cn_start_idx + N_bins_linespan - 1;
-
+    
+    global_start_idx = target_center_idx - span_bins;
+    global_end_idx   = target_center_idx + span_bins - 1;
+    
     if global_start_idx > 0 && global_end_idx <= total_bins
-        spec_recon(global_start_idx:global_end_idx) = spec_recon(global_start_idx:global_end_idx) + Cn(Cn_start_idx: Cn_end_idx);
+        spec_recon(global_start_idx:global_end_idx) = Cn(Cn_start_idx:Cn_end_idx);
     else
         warning('Comb line n=%d is outside the spec_recon array bounds.', n);
     end
 end
-spec_recon = 20*log10(abs(spec_recon));
+
+% Plotting
+spec_recon_dB = 20*log10(abs(spec_recon));
+
 figure;
-plot(global_freq_axis / 1e12, spec_recon, "LineWidth",1.5);
+plot(global_freq_axis / 1e12, spec_recon_dB, "LineWidth", 1.5);
 title('Stitched Dual-Comb Spectrum');
 xlabel('Optical Frequency (THz)');
 ylabel('Signal Power (dB)')
-ylim([max(spec_recon)-60 max(spec_recon)+10]);
-xline(f_center / 1e12,'--b', "Center freq");
-xline(f_sig / 1e12,'--r', "Signal freq");
-set(gca,"Fontsize",18)
+ylim([max(spec_recon_dB)-60 max(spec_recon_dB)+10]);
+xline(f_center / 1e12, '--b', "Baseband Center", 'LabelVerticalAlignment', 'bottom');
+xline(f_s_global / 1e12, '--r', "Signal freq", 'LabelVerticalAlignment', 'bottom');
+set(gca, "Fontsize", 14)
